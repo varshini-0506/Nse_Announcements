ba8d9c5bb45cb10781163b8c9d997149478ceb57
Varshini <varshiniprabakaran0506@gmail.com>
2025-12-12 13:37:02 +0530

    added2

--- app.py
+++ app.py
@@ -233,5 +233,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
